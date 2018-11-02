81356b9efc1b8f7a416faea83689c3ff7c4b3bb5
Charley Ferrari <charley@charleyferrari.com>
2018-11-01 23:30:44 -0400

    check if parents in tree, and solve bug with childAndParentRelationships

--- fs_api_helpers.py
+++ fs_api_helpers.py
@@ -33,7 +33,6 @@ def retrieve_person(cookie, person_id):
 
 def recurse_tree(G, center, cookie, distance, origin):
     r = retrieve_person(cookie, center)
-    relationships = r.json()['childAndParentsRelationships'][0]
     print(r.json()['persons'][0]['names'][0]['nameForms'][0]['fullText'])
     print('shortest path to origin = '+str(len(nx.shortest_path(G, source=origin, target=center))))
 
@@ -46,25 +45,36 @@ def recurse_tree(G, center, cookie, distance, origin):
     centerEqualsOrigin = center == origin
 
     # mother or father
-    if 'father' in relationships.keys():
-        if relationships['father']['resourceId'] in G.nodes:
-            father = False
-        else:
-            father = True
-    else:
-        father = False
-    if 'mother' in relationships.keys():
-        if relationships['mother']['resourceId'] in G.nodes:
-            mother = False
-        else:
-            mother = True
-    else:
-        mother = False
+    relationships = r.json()['childAndParentsRelationships'][0]
+    relationships = None
+    for relationship in r.json()['childAndParentsRelationships']:
+        if relationship['child']['resourceId'] == center:
+            relationships = relationship
+            break
 
-    if father:
-        relationship_type = 'father'
-    elif mother:
-        relationship_type = 'mother'
+    if not relationships:
+        mother = False
+        father = False
+    else:
+        if 'father' in relationships.keys():
+            if 'father' in [G.get_edge_data(*e)['relationship'] for e in G.out_edges(center)]:
+                father = False
+            else:
+                father = True
+        else:
+            father = False
+        if 'mother' in relationships.keys():
+            if 'mother' in [G.get_edge_data(*e)['relationship'] for e in G.out_edges(center)]:
+                mother = False
+            else:
+                mother = True
+        else:
+            mother = False
+
+        if father:
+            relationship_type = 'father'
+        elif mother:
+            relationship_type = 'mother'
 
     if tooFarUp:
         print('too far up')
@@ -73,24 +83,29 @@ def recurse_tree(G, center, cookie, distance, origin):
         if father or mother:
             print('adding '+relationship_type)
             pid = relationships[relationship_type]['resourceId']
-            r = retrieve_person(cookie, pid)
-            name = r.json()['persons'][0]['names'][0]['nameForms'][0]['fullText']
-            birthplace = ''
-            for fact in r.json()['persons'][0]['facts']:
-                if fact['type'] == 'http://gedcomx.org/Birth':
-                    if 'place' in fact.keys():
-                        birthplace = fact['place']['original']
-                        break
-            # Check Mauritius
-            if 'mauritius' in birthplace.lower():
-                print('#######################')
-                print(name + ' ' + pid + ' WAS BORN IN MAURITIUS')
-                print('#######################')
-            ####################
+            if pid in G.nodes:
+                print(relationship_type+' already in tree')
+                G.add_edge(center, pid, relationship=relationship_type)
+                return recurse_tree(G, center, cookie, distance, origin)
+            else:
+                r = retrieve_person(cookie, pid)
+                name = r.json()['persons'][0]['names'][0]['nameForms'][0]['fullText']
+                birthplace = ''
+                for fact in r.json()['persons'][0]['facts']:
+                    if fact['type'] == 'http://gedcomx.org/Birth':
+                        if 'place' in fact.keys():
+                            birthplace = fact['place']['original']
+                            break
+                # Check Mauritius
+                if 'mauritius' in birthplace.lower():
+                    print('#######################')
+                    print(name + ' ' + pid + ' WAS BORN IN MAURITIUS')
+                    print('#######################')
+                ####################
 
-            G.add_node(pid, name=name, birthplace=birthplace)
-            G.add_edge(center, pid, relationship=relationship_type)
-            return recurse_tree(G, pid, cookie, distance, origin)
+                G.add_node(pid, name=name, birthplace=birthplace)
+                G.add_edge(center, pid, relationship=relationship_type)
+                return recurse_tree(G, pid, cookie, distance, origin)
         else:
             if centerEqualsOrigin:
                 print('done recursion')
